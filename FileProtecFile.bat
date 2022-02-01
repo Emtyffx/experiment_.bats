417ad57aa91d9f040d7bbf1bbb966a158b3f9d20
pavlo393 <75797160+pavlo393@users.noreply.github.com>
2022-02-01 21:06:46 +0200

    Add files via upload

--- FileProtecFile.bat
+++ FileProtecFile.bat
@@ -1,10 +1,9 @@
-@ECHO ON
+@ECHO OFF
 title ASP.io site protocol breeding mammoth
-::-------------------------------------------------------------------------------------------------------------------------------------------------------
 goto start
 :start
-
-set /p b=="How many: "
-for /l %%n in (1, 1, %b%) do noob >> mammoth%%n.txt 
+set /p b="How many: "
+set /p t="Name: "
+for /l %%n in (1, 1, %b%) do dir >> %t%+%%n.txt 
 echo hello
  
