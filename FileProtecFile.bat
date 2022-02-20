11dca64082cfd172b5c880597ca6cac1e17e2f7d
pavlo393 <75797160+pavlo393@users.noreply.github.com>
2022-02-20 14:53:53 +0200

    Update FileProtecFile.bat

--- FileProtecFile.bat
+++ FileProtecFile.bat
@@ -2,8 +2,9 @@
 title ASP.io site protocol breeding mammoth
 goto start
 :start
+set /p v="Command: "
 set /p b="How many: "
 set /p t="Name: "
-for /l %%n in (1, 1, %b%) do dir >> %t%+%%n.txt 
+for /l %%n in (1, 1, %b%) do %v% >> %t%%%n.txt 
 echo hello
  
